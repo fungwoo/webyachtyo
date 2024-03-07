bc2112ef23b8202ab90e3f4a40a85014a88a7c68
fungwoo <159402400+fungwoo@users.noreply.github.com>
2024-03-07 22:35:44 +0900

    Add files via upload

---
+++ js/script_homebtn.js
@@ -0,0 +1,13 @@
+$(window).scroll(function () {
+    var height = $(window).scrollTop();
+    if (height > 1000) {
+        /*아이콘이 나타나길 원하는 위치를 
+        화면상의 높이(=window상의 header부터 아래로, 여기서는 1000으로 했다)로 
+        설정한다.*/
+        $(".home_btn").fadeIn();
+        /*나타날 아이콘이 있는 클래스(홈버튼이 있는 클래스)로 지정*/
+    } else {
+        $(".home_btn").fadeOut();
+        /*나타날 아이콘이 있는 클래스(홈버튼이 있는 클래스)로 다시 지정*/
+    }
+});
